b15b5a837362a5101127726ced9e70f1a369f5f0
WilliamHsieh86 <48529570+WilliamHsieh86@users.noreply.github.com>
2022-03-21 12:50:55 -0400

    Added temperature measurement and no delay

--- Arduino_LIS3DS.ino
+++ Arduino_LIS3DS.ino
@@ -6,9 +6,12 @@ LIS3DH myIMU; //Default constructor is I2C, addr 0x19.
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(10, OUTPUT);
+  analogWrite(10, 127);
+  analogWrite(11, 0);
   Serial.begin(9600);
-  delay(1000); //relax...
-  Serial.println("Processor came out of reset.\n");
+  delay(1000);//relax...
+  //Serial.println("Processor came out of reset.\n");
 
   //Call .begin() to configure the IMU
   myIMU.begin();
@@ -16,21 +19,34 @@ void setup() {
 }
 
 int runCount =0;
+int temp = 0;
+int new_temp = 0;
+int actual_temp = 0;
 void loop()
 {
   //Get all parameters
   //Serial.print("\nAccelerometer:\n");
- // Serial.print(" X = ");
+  
   Serial.print(runCount++);
   Serial.print(",");
+  Serial.print(" X = ");
   Serial.print(myIMU.readFloatAccelX(), 4);
   Serial.print(",");
-  //Serial.print(" Y = ");
+  Serial.print(" Y = ");
   Serial.print(myIMU.readFloatAccelY(), 4);
   Serial.print(",");
-  //Serial.print(" Z = ");
+  Serial.print(" Z = ");
   Serial.print(myIMU.readFloatAccelZ(), 4);
+  Serial.print(",");
+  //read temp test
+  
+  temp = int((myIMU.read10bitADC3()>>6));
+  new_temp = 19 - temp;
+  actual_temp = new_temp + 19;
+  //temp = 45- temp;
+   Serial.print(" T = ");
+  Serial.print(actual_temp);
   Serial.print('\n');
 
-  delayMicroseconds(10);
+  //delayMicroseconds(1000);
 }
